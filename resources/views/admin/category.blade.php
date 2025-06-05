d691f42989a64832421696ecd0ef581bd0e3ddd8
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-05 13:42:08 +0700

    Sweet Alert Delete Confirmation

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -69,7 +69,7 @@
                     <tr>
                         <td>{{$category->category}}</td>
                         <td>
-                            <a class="btn btn-danger" href="{{ url('/delete-category', $category->id) }}">Delete</a>
+                            <a class="btn btn-danger" href="{{ url('/delete-category', $category->id) }}" onclick="confirmation(event)">Delete</a>
                         </td>
                     </tr>
                     @endforeach
@@ -78,6 +78,27 @@
         </div>
     </div>
     <!-- JavaScript files-->
+
+    <script type="text/javascript">
+        function confirmation(ev) {
+            ev.preventDefault();
+            
+            var urlToRedirect = ev.currentTarget.getAttribute('href');
+
+            swal({
+                title: 'Are You Sure to Delete This',
+                text: 'This Delete will be Permanent',
+                icon: 'warning',
+                buttons: true,
+                dengerMode: true
+            }).then((willCancel)=>{
+                if (willCancel) {
+                    window.location.href=urlToRedirect;
+                }
+            });
+        }
+    </script>
+
     <script src="{{asset('/template/admin/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('/template/admin/vendor/popper.js/umd/popper.min.js')}}"> </script>
     <script src="{{asset('/template/admin/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
@@ -86,5 +107,6 @@
     <script src="{{asset('/template/admin/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
     <script src="{{asset('/template/admin/js/charts-home.js')}}"></script>
     <script src="{{asset('/template/admin/js/front.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   </body>
 </html>
