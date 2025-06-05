50235d51a7ee3b85dffc993bd3b82a2816a37096
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-05 11:35:08 +0700

    Toastr Message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,9 +18,15 @@ class AdminController extends Controller
     }
 
     public function addCategory(Request $request) {
+        if (empty($request->category)) {
+            toastr()->closeButton()->error('Category Required');
+            return redirect()->back();
+        }
+
         Category::create([
             'category' => $request->category
         ]);
+        toastr()->closeButton()->addSuccess('Category Added Successfully');
         return redirect()->back();
     }
 }
